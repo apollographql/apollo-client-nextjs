2800a579fb703ebd8112366eb45c3efc2a582f91
Lenz Weber-Tronic <lenz@apollographql.com>
2023-03-17 12:56:46 +0100

    move experimental code over

--- package/.eslintrc.js
+++ package/.eslintrc.js
@@ -17,5 +17,7 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+  },
 };
