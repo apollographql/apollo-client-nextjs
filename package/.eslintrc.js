47cc112d31b60f6b732ba5181fce1569f5b5bca8
Lenz Weber-Tronic <lenz@apollographql.com>
2023-03-17 12:37:17 +0100

    initial commit

---
+++ package/.eslintrc.js
@@ -0,0 +1,21 @@
+/** @type {import("@types/eslint").Linter.Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
