3a9eadc5f39e2074233090170eadc4c7d7554ee4
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-05-09 12:10:41 +0200

    use `useSuspenseQuery` to make use of streaming SSR

--- examples/polls-demo/app/ssr/poll-ssr.tsx
+++ examples/polls-demo/app/ssr/poll-ssr.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useQuery } from "@apollo/experimental-nextjs-app-support/ssr";
+import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
 import { useMutation } from "@apollo/client";
 import { Poll as PollInner } from "@/components/poll";
 import { pollQuery } from "@/components/poll/query";
@@ -8,7 +8,7 @@ import { useCallback } from "react";
 import { answerPollMutation } from "@/components/poll/mutation";
 
 export const Poll = () => {
-  const { data, loading, error } = useQuery(pollQuery, {
+  const { data } = useSuspenseQuery(pollQuery, {
     variables: { id: "1" },
   });
 
@@ -23,14 +23,6 @@ export const Poll = () => {
     },
     [mutate]
   );
-
-  if (loading) {
-    return <p>Loading...</p>;
-  } else if (error) {
-    return <p>Error: {error.message}</p>;
-  } else if (!data) {
-    return <p>No data</p>;
-  }
 
   return (
     <PollInner
