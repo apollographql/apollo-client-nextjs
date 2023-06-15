a0c75a8d7ff3b432dcdb234e3b07bd95baea1990
alessia <alessia@apollographql.com>
2023-06-15 09:42:58 -0400

    chore: use beta 3 everywhere

--- package/src/ssr/ApolloRehydrateSymbols.tsx
+++ package/src/ssr/ApolloRehydrateSymbols.tsx
@@ -11,7 +11,6 @@ declare global {
     [ApolloRehydrationCache]?: RehydrationCache;
     [ApolloResultCache]?: ResultsCache;
     [ApolloSSRDataTransport]?: DataTransport<SuperJSONResult>;
-    // TODO: types
     [ApolloBackgroundQueryTransport]?: BackgroundQueriesCache;
   }
 }
