3dff6046759205d9994c162dfd729317a716b3f5
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-06-21 10:40:44 +0200

    test helper: reset more values (#40)

--- package/src/ssr/testHelpers.ts
+++ package/src/ssr/testHelpers.ts
@@ -1,15 +1,25 @@
-import { ApolloRehydrationCache, ApolloResultCache, ApolloSSRDataTransport } from "./ApolloRehydrateSymbols";
+import {
+  ApolloClientSingleton,
+  SuspenseCacheSingleton,
+} from "./ApolloNextAppProvider";
+import {
+  ApolloRehydrationCache,
+  ApolloResultCache,
+  ApolloSSRDataTransport,
+} from "./ApolloRehydrateSymbols";
 
 /**
  * Resets the singleton instances created for the Apollo SSR data transport and caches.
- * 
+ *
  * To be used in testing only, like
  * ```ts
  * afterEach(resetNextSSRApolloSingletons);
  * ```
  */
-export function resetNextSSRApolloSingletons(){
-    delete window[ApolloRehydrationCache];
-    delete window[ApolloResultCache];
-    delete window[ApolloSSRDataTransport];
+export function resetNextSSRApolloSingletons() {
+  delete window[ApolloRehydrationCache];
+  delete window[ApolloResultCache];
+  delete window[ApolloSSRDataTransport];
+  delete window[ApolloClientSingleton];
+  delete window[SuspenseCacheSingleton];
 }
