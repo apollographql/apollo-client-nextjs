b3bcad849460a545437f69e6a6c684a3cbe3dc5c
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-06-27 13:31:25 +0200

    `await navigation` in tests

--- integration-test/src/app/cc/dynamic/apollo-client.test.ts
+++ integration-test/src/app/cc/dynamic/apollo-client.test.ts
@@ -3,16 +3,24 @@ import { test } from "../../../../fixture";
 
 test.describe("CC dynamic", () => {
   test("useSuspenseQuery (one query)", async ({ page, blockRequest }) => {
-    page.goto("http://localhost:3000/cc/dynamic/useSuspenseQuery");
+    const naviagtion = page.goto(
+      "http://localhost:3000/cc/dynamic/useSuspenseQuery"
+    );
 
     await expect(page.getByText("Soft Warm Apollo Beanie")).toBeVisible();
+
+    await naviagtion;
   });
 
   test("useBackgroundQuery (one query)", async ({ page, blockRequest }) => {
-    page.goto("http://localhost:3000/cc/dynamic/useBackgroundQuery");
+    const naviagtion = page.goto(
+      "http://localhost:3000/cc/dynamic/useBackgroundQuery"
+    );
 
     await expect(page.getByText("loading")).toBeVisible();
     await expect(page.getByText("loading")).not.toBeVisible();
     await expect(page.getByText("Soft Warm Apollo Beanie")).toBeVisible();
+
+    await naviagtion;
   });
 });
--- integration-test/src/app/rsc/static/apollo-client.test.ts
+++ integration-test/src/app/rsc/static/apollo-client.test.ts
@@ -3,8 +3,12 @@ import { test } from "../../../../fixture";
 
 test.describe("RSC static", () => {
   test("useSuspenseQuery (one query)", async ({ page, blockRequest }) => {
-    page.goto("http://localhost:3000/rsc/static/useSuspenseQuery");
+    const navigation = page.goto(
+      "http://localhost:3000/rsc/static/useSuspenseQuery"
+    );
 
     await expect(page.getByText("Soft Warm Apollo Beanie")).toBeVisible();
+
+    await navigation;
   });
 });
