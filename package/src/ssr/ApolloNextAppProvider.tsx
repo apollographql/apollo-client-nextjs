02faed3d36ec4377e0ecb7b26cf13df86fdbe5fb
Jerel Miller <jerelmiller@gmail.com>
2023-07-18 03:53:02 -0600

    Update `@apollo/client` peer dependency to `rc` and remove `makeSuspenseCache` (#61)
    * Require `@apollo/client@rc` and drop suspense cache
    
    * Update examples and integration tests to use rc version
    
    * Update README with instructions to use `rc` version.
    
    * Remove rogue `SuspenseCache` import in README
    
    * Remove `SuspenseCacheSingleton` from test helpers
    
    * bump internal deps to `>=3.8.0-rc.1`
    
    ---------
    
    Co-authored-by: Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>

--- package/src/ssr/ApolloNextAppProvider.tsx
+++ package/src/ssr/ApolloNextAppProvider.tsx
@@ -2,53 +2,35 @@
 import * as React from "react";
 import {
   ApolloClient,
-  SuspenseCache,
   ApolloProvider as _ApolloProvider,
 } from "@apollo/client";
 import { RehydrationContextProvider } from "./RehydrationContext";
 
 export const ApolloClientSingleton = Symbol.for("ApolloClientSingleton");
-export const SuspenseCacheSingleton = Symbol.for(
-  "ApolloSuspenseCacheSingleton"
-);
 
 declare global {
   interface Window {
     [ApolloClientSingleton]?: ApolloClient<any>;
-    [SuspenseCacheSingleton]?: SuspenseCache;
   }
 }
 export const ApolloNextAppProvider = ({
   makeClient,
   children,
-  makeSuspenseCache,
 }: React.PropsWithChildren<{
   makeClient: () => ApolloClient<any>;
-  makeSuspenseCache?: () => SuspenseCache;
 }>) => {
   const clientRef = React.useRef<ApolloClient<any>>();
-  const suspenseCacheRef = React.useRef<SuspenseCache>();
 
   if (typeof window !== "undefined") {
     clientRef.current = window[ApolloClientSingleton] ??= makeClient();
-    suspenseCacheRef.current = window[SuspenseCacheSingleton] ??=
-      makeSuspenseCache?.();
   } else {
     if (!clientRef.current) {
       clientRef.current = makeClient();
     }
-    if (!suspenseCacheRef.current && makeSuspenseCache) {
-      suspenseCacheRef.current = makeSuspenseCache();
-    }
   }
 
   return (
-    <_ApolloProvider
-      client={clientRef.current}
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore This is there in the betas and gone in the RC. Will be removed next version.
-      suspenseCache={suspenseCacheRef.current}
-    >
+    <_ApolloProvider client={clientRef.current}>
       <RehydrationContextProvider>{children}</RehydrationContextProvider>
     </_ApolloProvider>
   );
