6fa15516213385b305e762da88382109e713ebb0
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-09-20 11:44:34 +0200

    update next version in examples & integration tests

--- examples/polls-demo/next.config.js
+++ examples/polls-demo/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+  experimental: {},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
