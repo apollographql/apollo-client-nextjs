51e2f6015ec4f3691ce7fc34d43070364b44a73f
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-02-09 17:38:34 +0100

    remove accidentally committed file

--- package/run-tests.ts
+++
@@ -1,9 +0,0 @@
-import { run } from "node:test";
-import { spec } from "node:test/reporters";
-import process from "node:process";
-
-import { glob } from "glob";
-
-const tsTests = await glob("src/**/*.test.ts");
-
-run({ files: tsTests }).compose(new spec()).pipe(process.stdout);
