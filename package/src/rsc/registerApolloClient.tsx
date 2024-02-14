974b37d57307f32471a0f9744f38455dcfa96cdf
monssef <m-heroine@hotmail.com>
2024-02-15 00:30:13 +0800

    add parameter type support to getClient
    i kept getting `getClient Expected 0 arguments, but got ...` error

--- package/src/rsc/registerApolloClient.tsx
+++ package/src/rsc/registerApolloClient.tsx
@@ -3,7 +3,7 @@ import "server-only";
 import type { ApolloClient } from "@apollo/client";
 import * as React from "react";
 
-export function registerApolloClient(makeClient: () => ApolloClient<any>) {
+export function registerApolloClient<T extends any[]>(makeClient: (...args: T) => ApolloClient<any>) {
   const getClient = React.cache(makeClient);
   return {
     getClient,
