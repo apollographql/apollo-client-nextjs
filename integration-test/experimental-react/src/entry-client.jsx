b5235201d77c1b6538e4479ad1e9283b54b4fac2
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-02-14 14:17:09 +0100

    initialize basic vite react-ssr project

---
+++ integration-test/experimental-react/src/entry-client.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.hydrateRoot(
+  document.getElementById('root'),
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
