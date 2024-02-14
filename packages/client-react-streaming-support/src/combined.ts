8ffb9ce675e8f239b7330f14b7d0150d2eaa4663
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-02-14 14:01:12 +0100

    add some explaining comments

--- packages/client-react-streaming-support/src/combined.ts
+++ packages/client-react-streaming-support/src/combined.ts
@@ -1,2 +1,17 @@
+/**
+ * TypeScript does not have the concept of these environments,
+ * so we need to create a single entry point that combines all
+ * possible exports.
+ * That means that users will be offered "RSC" exports in a
+ * "SSR/Browser" code file, but those will error in a compilation
+ * step.
+ *
+ * This is a limitation of TypeScript, and we can't do anything
+ * about it.
+ *
+ * The build process will only create `.d.ts`/`d.cts` files from
+ * this, and not actual runtime code.
+ */
+
 export * from "./index.rsc.js";
 export * from "./index.js";
--- packages/client-react-streaming-support/src/empty.ts
+++ packages/client-react-streaming-support/src/empty.ts
@@ -1,1 +1,7 @@
+/**
+ * An empty file that will be used for export conditions on paths where they should just not have a match.
+ *
+ * E.g. importing from /ssr in a RSC file will end up being served this file.
+ */
+
 export {};
