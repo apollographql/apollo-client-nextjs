fbbd1528b32f4e668be05fc30c4d074a0b8b2e24
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-02-14 14:28:16 +0100

    apply prettier

--- integration-test/experimental-react/src/entry-client.jsx
+++ integration-test/experimental-react/src/entry-client.jsx
@@ -1,11 +1,11 @@
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 ReactDOM.hydrateRoot(
-  document.getElementById('root'),
+  document.getElementById("root"),
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
--- integration-test/experimental-react/src/entry-server.jsx
+++ integration-test/experimental-react/src/entry-server.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
-import App from './App'
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import App from "./App";
 
 export function render() {
   const html = ReactDOMServer.renderToString(
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  )
-  return { html }
+  );
+  return { html };
 }
