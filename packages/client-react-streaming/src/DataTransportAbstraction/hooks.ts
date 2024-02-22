3bb5b5f6e393aab243c6631ca1ef80dc88220565
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-02-22 14:44:15 +0100

    wrap upstream package hooks with streaming functionality

--- packages/client-react-streaming/src/DataTransportAbstraction/hooks.ts
+++ packages/client-react-streaming/src/DataTransportAbstraction/hooks.ts
@@ -1,43 +1,68 @@
 "use client";
+import type { ApolloClient } from "@apollo/client/index.js";
 import {
-  useFragment as orig_useFragment,
-  useSuspenseQuery as orig_useSuspenseQuery,
-  useReadQuery as orig_useReadQuery,
-  useQuery as orig_useQuery,
-  useBackgroundQuery as orig_useBackgroundQuery,
+  useApolloClient,
+  useFragment,
+  useSuspenseQuery,
+  useReadQuery,
+  useQuery,
+  useBackgroundQuery,
 } from "@apollo/client/index.js";
+import { wrapFunction } from "@apollo/client/utilities/internal/index.js";
 import { useTransportValue } from "./useTransportValue.js";
+import { WrappedApolloClient } from "./WrappedApolloClient.js";
 
-export const useFragment = wrap(orig_useFragment, [
-  "data",
-  "complete",
-  "missing",
-]);
-export const useQuery = wrap<typeof orig_useQuery>(
+export {
+  useFragment,
+  useSuspenseQuery,
+  useReadQuery,
+  useQuery,
+  useBackgroundQuery,
+};
+
+wrap(
+  useFragment,
+  ["data", "complete", "missing"],
+  (options) => options?.client
+);
+wrap<typeof useQuery>(
   process.env.REACT_ENV === "ssr"
     ? (query, options) =>
-        orig_useQuery(query, { ...options, fetchPolicy: "cache-only" })
-    : orig_useQuery,
-  ["data", "loading", "networkStatus", "called"]
+        useQuery(query, { ...options, fetchPolicy: "cache-only" })
+    : useQuery,
+  ["data", "loading", "networkStatus", "called"],
+  (_, options) => options?.client
 );
-export const useSuspenseQuery = wrap(orig_useSuspenseQuery, [
-  "data",
-  "networkStatus",
-]);
-export const useReadQuery = wrap(orig_useReadQuery, ["data", "networkStatus"]);
-
-export const useBackgroundQuery = orig_useBackgroundQuery;
+wrap(useSuspenseQuery, ["data", "networkStatus"], (_, options) =>
+  typeof options === "object" ? options.client : undefined
+);
+wrap(useReadQuery, ["data", "networkStatus"], () => undefined);
 
 function wrap<T extends (...args: any[]) => any>(
   useFn: T,
-  transportKeys: (keyof ReturnType<T>)[]
+  transportKeys: (keyof ReturnType<T>)[],
+  getClientFromArgs: (...args: Parameters<T>) => ApolloClient<any> | undefined
 ): T {
-  return ((...args: any[]) => {
-    const result = useFn(...args);
-    const transported: Partial<typeof result> = {};
-    for (const key of transportKeys) {
-      transported[key] = result[key];
-    }
-    return { ...result, ...useTransportValue(transported) };
-  }) as T;
+  return wrapFunction(
+    useFn,
+    (useFn) =>
+      ((...args: Parameters<T>) => {
+        let client;
+        try {
+          client = useApolloClient(getClientFromArgs(...args));
+        } catch {
+          /** nothing to do */
+        }
+        if (client && client instanceof WrappedApolloClient) {
+          const result = useFn(...args);
+          const transported: Partial<typeof result> = {};
+          for (const key of transportKeys) {
+            transported[key] = result[key];
+          }
+          return { ...result, ...useTransportValue(transported) };
+        }
+        // if this is called with a non-wrapped client (maybe in a test?), just call the original hook
+        return useFn(...args);
+      }) as T
+  );
 }
