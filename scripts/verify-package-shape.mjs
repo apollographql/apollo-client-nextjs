41a9ea79909864305820363398058e5a20100f99
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-02-28 15:53:47 +0100

    check "package shapes"

---
+++ scripts/verify-package-shape.mjs
@@ -0,0 +1,71 @@
+// @ts-check
+
+import assert from "node:assert";
+import { exec } from "node:child_process";
+import { readFile } from "node:fs/promises";
+import { dirname } from "node:path";
+import { parseArgs } from "node:util";
+
+const args = parseArgs({
+  args: process.argv.slice(2),
+  allowPositionals: true,
+});
+
+for (const pkg of args.positionals) {
+  console.log("Checking package.json: %s", pkg);
+  await checkPackage(pkg);
+}
+
+async function checkPackage(/** @type {string} */ pkg) {
+  const json = JSON.parse(await readFile(pkg, { encoding: "utf-8" }));
+  for (const entryPoint of Object.keys(json)) {
+    for (const [condition, shape] of Object.entries(json[entryPoint])) {
+      shape.sort();
+      await verifyESM(condition, entryPoint, pkg, shape);
+      await verifyCJS(condition, entryPoint, pkg, shape);
+    }
+  }
+}
+/**
+ *
+ * @param {string} condition
+ * @param {string} entryPoint
+ * @param {string} pkg
+ * @param {string[]} shape
+ */
+async function verifyESM(condition, entryPoint, pkg, shape) {
+  console.log(`Checking ESM: ${entryPoint} with condition ${condition}`);
+  const child = exec(
+    `node --input-type=module --conditions ${condition} --eval 'console.log(JSON.stringify(Object.keys(await import("${entryPoint}"))))'`,
+    {
+      cwd: dirname(pkg),
+    }
+  );
+  let result = "";
+  child.stdout?.on("data", (data) => (result += data.toString()));
+  child.stderr?.pipe(process.stderr);
+  await new Promise((resolve) => child.on("exit", resolve));
+  assert.deepStrictEqual(JSON.parse(result).sort(), shape);
+}
+
+/**
+ *
+ * @param {string} condition
+ * @param {string} entryPoint
+ * @param {string} pkg
+ * @param {string[]} shape
+ */
+async function verifyCJS(condition, entryPoint, pkg, shape) {
+  console.log(`Checking CJS: ${entryPoint} with condition ${condition}`);
+  const child = exec(
+    `node --input-type=commonjs --conditions ${condition} --eval 'console.log(JSON.stringify(Object.keys(require("${entryPoint}"))))'`,
+    {
+      cwd: dirname(pkg),
+    }
+  );
+  let result = "";
+  child.stdout?.on("data", (data) => (result += data.toString()));
+  child.stderr?.pipe(process.stderr);
+  await new Promise((resolve) => child.on("exit", resolve));
+  assert.deepStrictEqual(JSON.parse(result).sort(), shape);
+}
