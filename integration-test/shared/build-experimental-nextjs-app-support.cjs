73fe5e95f12371455d7674a82ab40d94e530d77b
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-03-01 11:09:57 +0100

    split up into "sub-monorepo"

---
+++ integration-test/shared/build-experimental-nextjs-app-support.cjs
@@ -0,0 +1,44 @@
+/*
+The integration tests need the latest version of the `@apollo/client-react-streaming` package.
+
+This script can be used with the `exec:` protocol (https://yarnpkg.com/protocol/exec) to build
+the package.
+*/
+
+const { execFileSync } = require("node:child_process");
+const { join, dirname } = require("node:path");
+
+const monorepoRoot = dirname(
+  require.resolve("monorepo", { paths: [process.env.INIT_CWD] })
+);
+const pathToArchive = join(execEnv.tempDir, "archive.tgz");
+
+execFileSync(
+  `yarn`,
+  [
+    `workspace`,
+    `@apollo/experimental-nextjs-app-support`,
+    `pack`,
+    `--out`,
+    pathToArchive,
+  ],
+  {
+    stdio: `inherit`,
+    cwd: monorepoRoot,
+  }
+);
+execFileSync(
+  `tar`,
+  [
+    `-x`,
+    `-z`,
+    `--strip-components=1`,
+    `-f`,
+    pathToArchive,
+    `-C`,
+    execEnv.buildDir,
+  ],
+  {
+    stdio: `inherit`,
+  }
+);
