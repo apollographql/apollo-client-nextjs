bf15207f2b71a62eef395fc9dea86a1bf385c64f
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-03-26 15:44:17 +0100

    disable semgrep

--- packages/client-react-streaming/src/util/hydrationTest.ts
+++ packages/client-react-streaming/src/util/hydrationTest.ts
@@ -1,5 +1,6 @@
 import { hydrateRoot } from "react-dom/client";
 
+/* eslint-disable */
 // prettier-ignore
 /** React completeSegment function */
 // @ts-expect-error This is React code.
@@ -7,8 +8,8 @@ export function $RS(a, b) { a = document.getElementById(a); b = document.getElem
 // prettier-ignore
 /** React completeBoundary function */
 // @ts-expect-error This is React code.
-// eslint-disable-next-line no-var
 export function $RC(b, c, e = undefined) { c = document.getElementById(c); c.parentNode.removeChild(c); var a = document.getElementById(b); if (a) { b = a.previousSibling; if (e) b.data = "$!", a.setAttribute("data-dgst", e); else { e = b.parentNode; a = b.nextSibling; var f = 0; do { if (a && 8 === a.nodeType) { var d = a.data; if ("/$" === d) if (0 === f) break; else f--; else "$" !== d && "$?" !== d && "$!" !== d || f++ } d = a.nextSibling; e.removeChild(a); a = d } while (a); for (; c.firstChild;)e.insertBefore(c.firstChild, a); b.data = "$" } b._reactRetry && b._reactRetry() } }
+/* eslint-enable */
 
 export function hydrateBody(
   initialChildren: Parameters<typeof hydrateRoot>[1],
@@ -20,11 +21,13 @@ export function hydrateBody(
 export function setBody(html: TemplateStringsArray) {
   if (html.length !== 1)
     throw new Error("Expected exactly one template string");
+  // nosemgrep
   document.body.innerHTML = html[0];
 }
 
 export function appendToBody(html: TemplateStringsArray) {
   if (html.length !== 1)
     throw new Error("Expected exactly one template string");
+  // nosemgrep
   document.body.insertAdjacentHTML("beforeend", html[0]);
 }
