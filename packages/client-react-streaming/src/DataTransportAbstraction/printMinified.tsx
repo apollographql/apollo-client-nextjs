8cf8c1135d9c2c2542ee638cfdc8c304be3e1c62
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-04-11 12:32:12 +0200

    use `stripIgnoredCharacters`

--- packages/client-react-streaming/src/DataTransportAbstraction/printMinified.tsx
+++ packages/client-react-streaming/src/DataTransportAbstraction/printMinified.tsx
@@ -1,15 +1,7 @@
 import type { DocumentNode } from "@apollo/client/index.js";
 import { print } from "@apollo/client/utilities/index.js";
+import { stripIgnoredCharacters } from "graphql";
 
 export function printMinified(query: DocumentNode): string {
-  return (
-    print(query)
-      // replace multi-spaces with single space
-      .replace(/\s{2,}/g, " ")
-      // remove spaces that are preceeded by braces
-      .replace(/(?<=[{}])\s+/g, "")
-      // remove spaces that are preceeding braces
-      .replace(/\s+(?=[{}])/g, "")
-      .trim()
-  );
+  return stripIgnoredCharacters(print(query));
 }
