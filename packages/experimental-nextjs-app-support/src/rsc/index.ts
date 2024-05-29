012afed0095e0900b04254b354567d0a8b57625d
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-05-29 10:49:02 +0200

    Add deprecation messages to moved exports. (#301)
    * Add deprecation messages to moved exports.
    
    * fixup
    
    * update wording as per review

--- packages/experimental-nextjs-app-support/src/rsc/index.ts
+++ packages/experimental-nextjs-app-support/src/rsc/index.ts
@@ -1,4 +1,29 @@
-export {
-  registerApolloClient,
-  type TransportedQueryRef,
+import {
+  registerApolloClient as _registerApolloClient,
+  type TransportedQueryRef as _TransportedQueryRef,
 } from "@apollo/experimental-nextjs-app-support";
+
+/**
+ * @deprecated
+ * This import has moved to `"@apollo/experimental-nextjs-app-support"`.
+ *
+ * Please update your import to
+ * ```ts
+ * import { registerApolloClient } from "@apollo/experimental-nextjs-app-support";
+ * ```
+ */
+export const registerApolloClient = _registerApolloClient;
+
+/**
+ * @deprecated
+ * This import has moved to `"@apollo/experimental-nextjs-app-support"`.
+ *
+ * Please update your import to
+ * ```ts
+ * import type { TransportedQueryRef } from "@apollo/experimental-nextjs-app-support";
+ * ```
+ */
+export type TransportedQueryRef<
+  TData = unknown,
+  TVariables = unknown,
+> = _TransportedQueryRef<TData, TVariables>;
