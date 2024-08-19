3b1a1ef8d526f9eec092c5a3039ab5f9accb3f05
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-08-19 11:26:47 +0200

    further attempts on reducing footprint

--- packages/client-react-streaming/src/index.cc.tsx
+++ packages/client-react-streaming/src/index.cc.tsx
@@ -1,36 +1,14 @@
 "use client";
 
-import type { ProgressEvent } from "./DataTransportAbstraction/DataTransportAbstraction.js";
-import type { ReactNode } from "react";
-import type { TransportedQueryRefOptions } from "./transportedQueryRef.js";
 import * as React from "react";
 
-let RealSimulatePreloadedQuery: typeof SimulatePreloadedQuery;
-export function SimulatePreloadedQuery<T>({
-  options,
-  result,
-  children,
-  queryKey,
-}: {
-  options: TransportedQueryRefOptions;
-  result: Promise<Array<Omit<ProgressEvent, "id">>>;
-  children: ReactNode;
-  queryKey?: string;
-}): ReactNode {
-  if (!RealSimulatePreloadedQuery) {
-    RealSimulatePreloadedQuery = React.lazy(() =>
-      import("./SimulatePreloadedQuery.cc.js").then((m) => ({
-        default: m.SimulatePreloadedQuery,
-      }))
-    );
-  }
-  return (
-    <RealSimulatePreloadedQuery
-      options={options}
-      result={result}
-      queryKey={queryKey}
-    >
-      {children}
-    </RealSimulatePreloadedQuery>
-  );
-}
+let RealSimulatePreloadedQuery: typeof import("./SimulatePreloadedQuery.cc.js").default;
+export const SimulatePreloadedQuery: typeof import("./SimulatePreloadedQuery.cc.js").default =
+  (props) => {
+    if (!RealSimulatePreloadedQuery) {
+      RealSimulatePreloadedQuery = React.lazy(
+        () => import("./SimulatePreloadedQuery.cc.js")
+      );
+    }
+    return <RealSimulatePreloadedQuery {...props} />;
+  };
