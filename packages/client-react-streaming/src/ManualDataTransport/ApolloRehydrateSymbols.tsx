e6bfd3a64c98600ca8c9b14765b2fbcf329e2694
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-10-21 09:47:34 +0200

    make the hook hydration cache resilient against unmounted recreation of `ManualDataTransport` (#375)

--- packages/client-react-streaming/src/ManualDataTransport/ApolloRehydrateSymbols.tsx
+++ packages/client-react-streaming/src/ManualDataTransport/ApolloRehydrateSymbols.tsx
@@ -1,10 +1,16 @@
 import type { DataTransport } from "./dataTransport.js";
+import type { RehydrationCache } from "./types.js";
 
 declare global {
   interface Window {
     [ApolloSSRDataTransport]?: DataTransport<unknown>;
+    [ApolloHookRehydrationCache]?: RehydrationCache;
   }
 }
 export const ApolloSSRDataTransport = /*#__PURE__*/ Symbol.for(
   "ApolloSSRDataTransport"
+);
+
+export const ApolloHookRehydrationCache = /*#__PURE__*/ Symbol.for(
+  "apollo.hookRehydrationCache"
 );
