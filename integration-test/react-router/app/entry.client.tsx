2b56da636f03b2081ac56686a4949b195e7fe4f1
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-11-25 11:07:54 +0100

    add raw experiment

--- integration-test/react-router/app/entry.client.tsx
+++ integration-test/react-router/app/entry.client.tsx
@@ -1,12 +1,19 @@
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
+import { makeClient } from "./apollo";
+import { ApolloProvider } from "@apollo/client/index.js";
 
 startTransition(() => {
+  // TODO
+  const client =
+    makeClient() as any as import("@apollo/client/index.js").ApolloClient<any>;
   hydrateRoot(
     document,
     <StrictMode>
-      <HydratedRouter />
+      <ApolloProvider client={client}>
+        <HydratedRouter />
+      </ApolloProvider>
     </StrictMode>
   );
 });
--- integration-test/react-router/app/routes/home.tsx
+++ integration-test/react-router/app/routes/home.tsx
@@ -1,13 +1,60 @@
+import { useLoaderData, type MetaFunction } from "react-router";
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import type { TypedDocumentNode } from "@apollo/client/index.js";
+import { gql, useReadQuery } from "@apollo/client/index.js";
+import { apolloLoader } from "~/apollo";
 
-export function meta({}: Route.MetaArgs) {
+export const meta: MetaFunction = () => {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
+};
+
+interface Posts {
+  posts: {
+    data: Array<{
+      id: string;
+      title: string;
+    }>;
+  };
 }
+const GET_POSTS: TypedDocumentNode<Posts> = gql`
+  query GetPosts {
+    posts(options: { paginate: { page: 1, limit: 5 } }) {
+      data {
+        id
+        title
+      }
+    }
+  }
+`;
+
+export const loader = apolloLoader<Route.LoaderArgs>()(({ preloadQuery }) => {
+  const postsRef = preloadQuery(GET_POSTS);
+  return {
+    postsRef,
+  };
+});
 
 export default function Home() {
-  return <Welcome />;
+  const { postsRef } = useLoaderData<typeof loader>();
+  const posts = useReadQuery(postsRef).data.posts.data;
+
+  return (
+    <div className="p-2 flex gap-2">
+      <ul className="list-disc pl-4">
+        {posts.map((post) => {
+          return (
+            <li key={post.id} className="whitespace-nowrap">
+              <div>{post.title.substring(0, 20)}</div>
+            </li>
+          );
+        })}
+      </ul>
+      <hr />
+      <Welcome />
+    </div>
+  );
 }
