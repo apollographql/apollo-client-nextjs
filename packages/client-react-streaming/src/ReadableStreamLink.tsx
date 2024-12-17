400846ac065ca9f6144d4eeb1c35b612ba477d68
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-12-17 20:18:15 +0100

    feedback from code review

--- packages/client-react-streaming/src/ReadableStreamLink.tsx
+++ packages/client-react-streaming/src/ReadableStreamLink.tsx
@@ -57,22 +57,22 @@ export const TeeToReadableStreamLink = new ApolloLink((operation, forward) => {
 
   if (controller) {
     return new Observable((observer) => {
-      const subscription = forward(operation).subscribe(
-        (result) => {
+      const subscription = forward(operation).subscribe({
+        next(result) {
           controller.enqueue({ type: "next", value: result });
           observer.next(result);
         },
-        (error) => {
+        error(error) {
           controller.enqueue({ type: "error" });
           controller.close();
           observer.error(error);
         },
-        () => {
+        complete() {
           controller.enqueue({ type: "completed" });
           controller.close();
           observer.complete();
-        }
-      );
+        },
+      });
 
       return () => {
         subscription.unsubscribe();
