e2d90b37fd25029f16cf2332c56e375ee61088f4
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-12-20 14:49:11 +0100

    streamline integration test build

--- integration-test/shared/build-client-integration-react-router.cjs
+++ integration-test/shared/build-client-integration-react-router.cjs
@@ -5,39 +5,22 @@ This script can be used with the `exec:` protocol (https://yarnpkg.com/protocol/
 the package.
 */
 
-const { execFileSync } = require("node:child_process");
-const { join, dirname } = require("node:path");
-const monorepoRoot = join(process.env.PROJECT_CWD, "..");
-const pathToArchive = join(execEnv.tempDir, "archive.tgz");
+const { join } = require("node:path");
+const { $, cd, retry, sleep } = /** @type {typeof import('zx')} */ (
+  require(require.resolve("zx", { paths: [process.env.PROJECT_CWD] }))
+);
 
-setTimeout(() => {
-  execFileSync(
-    `yarn`,
-    [
-      `workspace`,
-      `@apollo/client-integration-react-router`,
-      `pack`,
-      `--out`,
-      pathToArchive,
-    ],
-    {
-      stdio: `inherit`,
-      cwd: monorepoRoot,
-    }
-  );
-  execFileSync(
-    `tar`,
-    [
-      `-x`,
-      `-z`,
-      `--strip-components=1`,
-      `-f`,
-      pathToArchive,
-      `-C`,
-      execEnv.buildDir,
-    ],
-    {
-      stdio: `inherit`,
-    }
-  );
-}, 5000);
+$.stdio = "inherit";
+
+(async function run() {
+  const monorepoRoot = join(process.env.PROJECT_CWD, "..");
+  const archive = join(monorepoRoot, "packages/react-router/archive.tgz");
+
+  // give the main build script a chance to kick in
+  await sleep(1000);
+
+  await retry(30, "1s", () => $`test -f ${archive}`);
+
+  cd(monorepoRoot);
+  await $`tar -x -z --strip-components=1 -f ${archive} -C ${execEnv.buildDir}`;
+})();
