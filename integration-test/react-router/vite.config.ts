2feaf0ada42185a11fe08e0c2ff201fe77741670
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-01-08 14:14:24 +0100

    more vercel adjustments

--- integration-test/react-router/vite.config.ts
+++ integration-test/react-router/vite.config.ts
@@ -4,11 +4,21 @@ import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild, command }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: "./server/app.ts",
+        }
+      : undefined,
+  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
+  ssr: {
+    noExternal: command === "build" ? true : undefined,
+  },
   plugins: [reactRouter(), tsconfigPaths()],
-});
+}));
