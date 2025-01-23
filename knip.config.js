1a0910b0928bcb4211ac485aa8f300e4db454b14
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-01-23 12:07:54 +0100

    config tweaks

--- knip.config.js
+++ knip.config.js
@@ -3,22 +3,28 @@
 /** @type{import('knip').KnipConfig}*/
 const config = {
   workspaces: {
+    ".": {
+      ignoreBinaries: ["jq", "playwright"],
+      ignoreDependencies: [/@size-limit\/.*/, "prettier"],
+    },
     "packages/*": {
       entry: ["**/*.test.{ts,tsx}"],
       project: ["**/*.{ts,tsx}"],
+    },
+    "packages/client-react-streaming": {
+      entry: ["**/*.test.{ts,tsx}"],
+      project: ["**/*.{ts,tsx}"],
+      ignoreDependencies: ["tsx"],
     },
   },
   ignore: [
     "examples/**",
     "integration-test/**",
-    "packages/test-utils/**",
+    "packages/test-utils/*.d.ts",
     "scripts/**",
     "packages/client-react-streaming/api-extractor.d.ts",
     "packages/experimental-nextjs-app-support/api-extractor.d.ts",
   ],
-  ignoreBinaries: ["jq", "playwright"],
-  ignoreDependencies: [/@size-limit\/.*/, "prettier", "tsx", "semver", "next"],
-  ignoreUnresolved: ["#bundled"],
 };
 
 export default config;
