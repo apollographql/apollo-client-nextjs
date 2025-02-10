0a3c8a868e9d2a556676b9ea6dad1828cd550f23
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-02-10 14:45:39 +0100

    rename route

--- integration-test/tanstack-start/app/routes/useSuspenseQuery-defer.tsx
+++ integration-test/tanstack-start/app/routes/useSuspenseQuery-defer.tsx
@@ -1,18 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { DEFERRED_QUERY } from "@integration-test/shared/queries";
-import { useApolloClient, useSuspenseQuery } from "@apollo/client/index.js";
-import { useTransition } from "react";
+import { createFileRoute } from '@tanstack/react-router'
+import { DEFERRED_QUERY } from '@integration-test/shared/queries'
+import { useApolloClient, useSuspenseQuery } from '@apollo/client/index.js'
+import { useTransition } from 'react'
 
-export const Route = createFileRoute("/useSuspenseQuery")({
+export const Route = createFileRoute('/useSuspenseQuery-defer')({
   component: RouteComponent,
-});
+})
 
 function RouteComponent() {
-  const [refetching, startTransition] = useTransition();
-  const client = useApolloClient();
+  const [refetching, startTransition] = useTransition()
+  const client = useApolloClient()
   const { data, refetch } = useSuspenseQuery(DEFERRED_QUERY, {
     variables: { delayDeferred: 1000 },
-  });
+  })
 
   return (
     <>
@@ -21,11 +21,11 @@ function RouteComponent() {
           <li key={id}>
             {title}
             <br />
-            Rating:{" "}
-            <div style={{ display: "inline-block", verticalAlign: "text-top" }}>
-              {rating?.value || ""}
+            Rating:{' '}
+            <div style={{ display: 'inline-block', verticalAlign: 'text-top' }}>
+              {rating?.value || ''}
               <br />
-              {rating ? `Queried in ${rating.env} environment` : "loading..."}
+              {rating ? `Queried in ${rating.env} environment` : 'loading...'}
             </div>
           </li>
         ))}
@@ -42,17 +42,17 @@ function RouteComponent() {
                   fields: {
                     rating: () => null,
                   },
-                });
+                })
               }
             },
-          });
+          })
           startTransition(() => {
-            refetch();
-          });
+            refetch()
+          })
         }}
       >
-        {refetching ? "refetching..." : "refetch"}
+        {refetching ? 'refetching...' : 'refetch'}
       </button>
     </>
-  );
+  )
 }
