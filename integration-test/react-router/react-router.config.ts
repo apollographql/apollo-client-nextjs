b0a79333d1a8a29ee04c4cfcb64893ed2b51635f
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2025-02-12 13:03:48 +0100

    update config, add log message

--- integration-test/react-router/react-router.config.ts
+++ integration-test/react-router/react-router.config.ts
@@ -1,9 +1,15 @@
 import { vercelPreset } from "@vercel/react-router/vite";
-import type { Config } from "@react-router/dev/config";
+import type { Config, Preset } from "@react-router/dev/config";
+
+let presets: Array<Preset> = [];
+if (process.env.VERCEL) {
+  console.log("Adding Vercel preset");
+  presets.push(vercelPreset());
+}
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  presets: process.env.VERCEL ? [vercelPreset()] : [],
+  presets,
 } satisfies Config;
